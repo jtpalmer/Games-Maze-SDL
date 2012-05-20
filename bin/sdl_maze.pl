0f81c6b723fc693ec24b2aed77b366ff17fe5e48
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-20 17:23:18 -0400

    Add SDLPerl check for darwin

--- bin/sdl_maze.pl
+++ bin/sdl_maze.pl
@@ -1,13 +1,18 @@
 #!perl
-
 use strict;
 use warnings;
 
-package
-    sdl_maze;
+BEGIN {
+    if ( $^O eq 'darwin' && $^X ne 'SDLPerl' ) {
+        exec 'SDLPerl', $0, @ARGV or die "Failed to exec SDLPerl: $!";
+    }
+}
 
 use Games::Maze::SDL;
 
 # ABSTRACT: Play the game!
 
 Games::Maze::SDL->new->run(@ARGV);
+
+exit;
+
